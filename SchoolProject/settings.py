b62a89ca2aac35e9361a04c239fdf8801445c2b1
Goodness3-coder <gooddan.gd@gmail.com>
2026-09-20 13:51:53 +0100

    Add Render URL to ALLOWED_HOSTS

--- SchoolProject/settings.py
+++ SchoolProject/settings.py
@@ -11,7 +11,7 @@ load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['schoolproject-ji6g.onrender.com', 'localhost', '127.0.0.1']
 
 # ✅ App Registration
 INSTALLED_APPS = [
